6cb070596e8469f7e2d71d7cf7e8981a34021655
Joseph P <jpark041@gmail.com>
2019-07-15 03:53:47 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -11,35 +11,46 @@ def consolidate_cart(cart)
   new_cart
 end
 
-# def apply_coupons(cart, coupons)
-#   coupons.each do |coupon| 
-#     coupon.each do |attribute, value| 
-#       #this will get inserted into AVOCADO W/ COUPONS
-#       name = coupon[:item]
-      
-#       #if our cart includes the item in question, AND the number of these items in our cart is grater than or equal to the number of items required by our coupon, then execute the next line of code in our “if” statement
-#       if cart[name] && cart[name][:count] >= coupon[:num] 
-#         if cart["#{name} W/COUPON"] 
-#           cart["#{name} W/COUPON"][:count] += 1 
-#         else 
-#           cart["#{name} W/COUPON"] = {
-#             :price => coupon[:cost],
-#             :clearance => cart[name][:clearance],
-#             :count => 1
-#           } 
-#         end 
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
+end
+
+def apply_clearance(cart)
+  cart.each do |item, attribute_hash|
+    if attribute_hash[:clearance] == true
+      attribute_hash[:price] = (attribute_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
+end
+
+def checkout(cart, coupons)
+  consolidated_cart = consolidate_cart(cart)
+  cart_with_coupons_applied = apply_coupons(consolidated_cart, coupons)
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+
+  total = 0.0
+  cart_with_discounts_applied.keys.each do |item|
+    total += cart_with_discounts_applied[item][:price] * cart_with_discounts_applied[item][:count]
+  end
   
-#       cart[name][:count] -= coupon[:num] 
-#     end 
-#   end 
-# end 
-#   cart 
-# end
-
-# def apply_clearance(cart)
-#   # code here
-# end
-
-# def checkout(cart, coupons)
-#   # code here
-# end
+  total > 100.00 ? (total * 0.90).round : total
+end
